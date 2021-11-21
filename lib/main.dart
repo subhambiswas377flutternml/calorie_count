f28a3a0e34fcf80590cf0f82375a262a64d4b694
@subhambiswas377flutternml <subhambiswas129@gmail.com>
2021-11-22 03:36:52 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import './start_screens/name_screen.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return MaterialApp(
+      home:NameScreen(),
+    );
+  }
+}
---
+++ lib/start_screens/name_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class NameScreen extends StatelessWidget
+{
+  Widget build(BuildContext context)
+  {
+    return Scaffold();
+  }
+}
